fdde0074211a4cc2c08b4ca33b425778bdb655db
samriggs <samriggs>
2013-10-24 00:11:22 -0400

    Revert "*** empty log message ***"
    This reverts commit 88fc6f906de3becfd072bbfab2e509cd1c96f100.

--- hw1/gradientDescent.m
+++ hw1/gradientDescent.m
@@ -16,8 +16,13 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    % https://share.coursera.org/wiki/index.php/ML:Linear_Regression_with_Multiple_Variables
-    theta -= (alpha / m) * X' * (X*theta - y)
+
+
+
+
+
+
+
     % ============================================================
 
     % Save the cost J in every iteration    
