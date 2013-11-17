0e86dce6ce6dc41df3d4e237eabb5a6e984ca4d4
samriggs <samriggs>
2013-11-16 23:45:52 -0500

    completed cost function for logistic regression.

--- hw2/costFunction.m
+++ hw2/costFunction.m
@@ -20,9 +20,9 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
-
-
+J = -( log(sigmoid(X * theta))' * y + \ 
+      log(1 - sigmoid(X * theta))' * (1-y) )/m
+grad = (X' * (sigmoid(X * theta) - y))/m
 
 
 
